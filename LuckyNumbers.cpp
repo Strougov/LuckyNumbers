ef9758c1e66eb137610561683a47f7f496dab32f
SlowRiot <slowriot@voxelstorm.com>
2015-04-29 15:04:09 +0100

    cache max value of bOne, 3% improvement in cpu usage

--- LuckyNumbers.cpp
+++ LuckyNumbers.cpp
@@ -13,11 +13,13 @@ int main()
 
 		for( unsigned int s2One = s2Min, aOne = s3 - s2One; s2One <= s2Max; ++s2One, --aOne)
 		{
-			for( unsigned int bOne = std::max( 0u, s2One - 9u ), cOne = s2One - bOne; bOne <= std::min( 9u, s2One ); ++bOne, --cOne)
+		  unsigned int const bOne_max = std::min( 9u, s2One );
+			for( unsigned int bOne = std::max( 0u, s2One - 9u ), cOne = s2One - bOne; bOne <= bOne_max; ++bOne, --cOne)
 			{
 				for( unsigned int s2Two = s2Min, aTwo = s3 - s2Two; s2Two <= s2Max; ++s2Two, --aTwo)
 				{
-					for( unsigned int bTwo = std::max( 0u, s2Two - 9u ), cTwo = s2Two - bTwo; bTwo <= std::min( 9u, s2Two ); ++bTwo, --cTwo)
+				  unsigned int const bTwo_max = std::min( 9u, s2Two );
+					for( unsigned int bTwo = std::max( 0u, s2Two - 9u ), cTwo = s2Two - bTwo; bTwo <= bTwo_max; ++bTwo, --cTwo)
 					{
 						std::cout << "\t: " << aOne << bOne << cOne << aTwo << bTwo << cTwo << std::endl;
 
